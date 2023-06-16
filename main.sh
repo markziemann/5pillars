c8fc04ec2fdc78552f3642722fc766acb605f189
markziemann <mark.ziemann@gmail.com>
2023-06-16 23:47:53 +1000

    fix libxt issue

--- main.sh
+++ main.sh
@@ -4,6 +4,8 @@ git clone https://github.com/markziemann/5pillars.git
 
 cd 5pillars
 
+apt update && apt install libxt-dev -y
+
 Rscript -e 'install.packages(c("rmarkdown","rmdformats","RColorBrewer"))'
 
 Rscript -e 'rmarkdown::render("manuscript/5pillars.Rmd")'
