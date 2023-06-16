ac8fc6ec50dd1e17648ddc3a5c84359d16cb5aef
markziemann <mark.ziemann@gmail.com>
2023-06-16 23:58:48 +1000

    remove apt cmd

--- main.sh
+++ main.sh
@@ -4,8 +4,6 @@ git clone https://github.com/markziemann/5pillars.git
 
 cd 5pillars
 
-apt update && apt install libxt-dev -y
-
 Rscript -e 'install.packages(c("rmarkdown","rmdformats","RColorBrewer"))'
 
 Rscript -e 'rmarkdown::render("manuscript/5pillars.Rmd")'
