ded7883aab38ab9d986d3cd31bf126f2d6b8cf2c
markziemann <mark.ziemann@gmail.com>
2023-08-29 16:26:30 +1000

    rocker not working due to dependancy issue

--- test_build.sh
+++ test_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it rocker/rstudio:4.3 bash -c "wget https://raw.githubusercontent.com/markziemann/5pillars/main/main.sh ; bash main.sh"
+docker run -it bioconductor/bioconductor:RELEASE_3_17 bash -c "wget https://raw.githubusercontent.com/markziemann/5pillars/main/main.sh ; bash main.sh"
 docker cp $(docker ps -aql):/5pillars/manuscript/5pillars.html . \
   && echo Build successful \
   || echo Error Build Failed
