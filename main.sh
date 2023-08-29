230ed281b422792f1589ad3abae1abbef45440ed
markziemann <mark.ziemann@gmail.com>
2023-08-29 16:55:58 +1000

    update docs and main script

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it bioconductor/bioconductor:RELEASE_3_17 bash -c "wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/markziemann/5pillars/main/main.sh ; bash main.sh"
+docker run -it bioconductor/bioconductor:RELEASE_3_17 bash -c "wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/markziemann/5pillars/main/helper.sh ; bash helper.sh"
 docker cp $(docker ps -aql):/5pillars/manuscript/5pillars.html . \
   && echo Build successful \
   || echo Error Build Failed
